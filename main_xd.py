fe31835a2139c1a25b725be82efdf75a3195985b
John Doe <r.feiglewicz@gmail.com>
2023-06-27 22:36:38 +0200

    Zoptymalizowano algorytm o pamiec

--- main_xd.py
+++ main_xd.py
@@ -1,80 +1,37 @@
 import graph_parser
-graph, podnuza_list, stars = graph_parser.get_graph("input.txt")
+graph, podnoza_list, stars = graph_parser.get_graph("input.txt")
 
-# graph = {
-#     0: [1, 4, 6],
-#     1: [2],
-#     2: [3, 9],
-#     3: [],
-#     9: [],
-#     4: [5],
-#     5: [],
-#     6: [8, 7],
-#     8: [],
-#     7: [],
-# }
 
-# stars = {
-#     0: [0],  #preprocessing
-#     1: [0],
-#     2: [5],
-#     3: [4],
-#     4: [2],
-#     5: [1],
-#     6: [4],
-#     7: [5],
-#     8: [4],
-#     9: [3],
-# }
 
 visitedList = []  
-
-def indeksy_najwiekszych_elementow(list):
-    biggest = max(list)
-    indices = [i for i, x in enumerate(list) if x == biggest]
-    return indices
-
-def print_items_by_index(list, indices):
-    for indeks in indices:
-        print(list[indeks])
-
+path_length = None
+stars_count = None #przechowywujemy najwieksza liczbe gwiazdek
 def depthFirst(graph, currentVertex, visited):
+    global path_length
+    global stars_count
     visited.append(currentVertex)
     for vertex in graph[currentVertex]:
         if vertex not in visited:
-            depthFirst(graph, vertex, visited.copy()) 
-    visitedList.append(visited)
+            depthFirst(graph, vertex, visited.copy())
+    if visited[-1] in podnoza_list: #Sprawdzamy czy sciezka konczy sie na podnozu
+        if path_length == None: # dla pierwszego wywolania
+            visitedList.append(visited)
+            path_length = len(visited)
+            stars_count = sum([stars.get(vertex,vertex) for vertex in visited])
+        else:
+            if len(visited)< path_length: #sprawdzamy czy aktualna sciezka jest krotsza niz poprzednia
+                visitedList.clear()
+                visitedList.append(visited) #dodajemy element
+                stars_count = sum([stars.get(vertex,vertex) for vertex in visited])
+                path_length = len(visited)
+            elif len(visited) == path_length:
+                if stars_count < sum([stars.get(vertex,vertex) for vertex in visited]): #czyscimy tablice i dodajemy element z najwieksza liczba gwiazdek, i aktualizujemy stars_count
+                    visitedList.clear()
+                    visitedList.append(visited) #dodajemy element
+                    stars_count = sum([stars.get(vertex,vertex) for vertex in visited])
 
+              
 depthFirst(graph, "szczyt" , [])
 
-
-list_sciez_podn = []
-
-for list_i in visitedList:
-    if list_i[-1] in podnuza_list:
-        list_sciez_podn.append(list_i)
-
-
-
-smallest_length = min(len(element) for element in list_sciez_podn)
-smallest_lists = [element for element in list_sciez_podn if len(element) == smallest_length]
-
-new_list = [[stars.get(vertex, vertex) for vertex in element] for element in smallest_lists]
-
-new_list_v2 = [sum(sublist) for sublist in new_list]
-
-#sum_list = [sum(sublist) for sublist in new_list_v2]
-
-index_list = indeksy_najwiekszych_elementow(new_list_v2)
-
-
+print("Optymalna sciezka to: ")
 print(visitedList)
-print(list_sciez_podn)
-print(smallest_lists)
-print(new_list)
-print(new_list_v2)
-#print(sum_list)
-print(index_list)
-
-print("optymalne sciezki to: ")
-print_items_by_index(smallest_lists, index_list)
